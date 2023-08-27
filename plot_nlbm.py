ce4571a525b8219d1f2458f1d7d1f01afa2af8e8
carmeloevoli <carmelo.evoli@gssi.it>
2023-08-27 17:27:46 +0200

    improve BC

--- plot_nlbm.py
+++ plot_nlbm.py
@@ -12,26 +12,33 @@ kpc = 1e3 * pc # cm
 
 cLight = 3e10 # cm / s
 tau_G = 2. * Myr # s
-hdisk = 100. * pc
-R_G = 10. * kpc
-V_G = math.pi * np.power(R_G, 2.) * 2. * hdisk
-n_s = 2000. / V_G
-q_0 = 11.3e43 # TBD
-beta = 2.80
+tau_0 = 6. * Myr # s
+hdisk = 100. * pc # cm
+snrate = 1. / 50 / year # 1/s
+R_G = 10. * kpc # cm
+V_G = math.pi * np.power(R_G, 2.) * 2. * hdisk # cm^3
+E_0 = 10. # GeV
+sigmaCB = 40e-27 # cm^2
+zeta = 0.1
+
+# Free parameters
+E_CR = 7.0e55 # GeV
+p = 2.80
+n_H = 0.8 # 1/cm^3
+n_c = 1.1 # 1/cm^3
 
 def I_H():
-    E = np.logspace(1, 4, 1000)
+    E = np.logspace(0, 4, 1000)
     value = cLight / 4. / math.pi
-    value *= n_s * tau_G
-    value *= q_0 * np.power(E, -beta)
+    value *= snrate * tau_G / V_G
+    value *= E_CR / np.power(E_0, 2.) * (p - 2.) * np.power(E / E_0, -p)
     return E, value
     
 def B_C():
-    E = np.logspace(1, 4, 1000)
-    T = E / E[0]
-    value = 0.15 * np.power(T, -0.1 * np.log(T))
-    value += 0.02
-    return E, value
+    E = np.logspace(0, 4, 1000)
+    value_G = cLight * sigmaCB * tau_G * n_H * (E / E)
+    value_c = cLight * sigmaCB * n_c * tau_0 * np.power(E, -zeta * np.log(E))
+    return E, value_c, value_G
     
 def plot_data(ax, filename, slope, norm, fmt, color, label, zorder=1):
     E, y, err_stat_lo, err_stat_up, err_sys_lo, err_sys_up = np.loadtxt(filename,usecols=(0,1,2,3,4,5),unpack=True)
@@ -48,9 +55,9 @@ def plot_data(ax, filename, slope, norm, fmt, color, label, zorder=1):
                 
 def plot_BC():
     def set_axes(ax):
-        ax.set_xlabel('E [GeV]')
+        ax.set_xlabel('E [GeV/n]')
         ax.set_xscale('log')
-        ax.set_xlim([1e1, 1e4])
+        ax.set_xlim([1e0, 1e4])
         ax.set_ylabel(r'B/C')
         ax.set_yscale('log')
         #ax.set_ylim(ylim)
@@ -59,12 +66,13 @@ def plot_BC():
     ax = fig.add_subplot(111)
     set_axes(ax)
 
-    plot_data(ax, 'kiss_tables/AMS-02_B_C_kineticEnergyPerNucleon.txt', 0.33, 1., 'o', 'tab:blue', 'AMS-02')
-    plot_data(ax, 'kiss_tables/CALET_B_C_kineticEnergyPerNucleon.txt', 0.33, 1., 'o', 'tab:orange', 'CALET', 3)
-    plot_data(ax, 'kiss_tables/DAMPE_B_C_kineticEnergyPerNucleon.txt', 0.33, 1., 'o', 'tab:red', 'DAMPE', 3)
+    plot_data(ax, 'kiss_tables/AMS-02_B_C_kineticEnergyPerNucleon.txt', 0., 1., 'o', 'tab:olive', 'AMS-02')
+    plot_data(ax, 'kiss_tables/CALET_B_C_kineticEnergyPerNucleon.txt', 0., 1., 'o', 'tab:orange', 'CALET', 3)
+    plot_data(ax, 'kiss_tables/DAMPE_B_C_kineticEnergyPerNucleon.txt', 0., 1., 'o', 'tab:red', 'DAMPE', 3)
 
-    E, bc = B_C()
-    ax.plot(E, np.power(E, 0.3) * bc, 'tab:gray')
+    E, bc_c, bc_g = B_C()
+    ax.plot(E, bc_c + bc_g, 'tab:gray')
+    ax.plot(E, bc_g, 'tab:gray', linestyle=':')
 
     ax.legend(fontsize=17, loc='lower left')
     plt.savefig('NLBM_BC.pdf')
@@ -82,7 +90,7 @@ def plot_protons():
     ax = fig.add_subplot(111)
     set_axes(ax)
 
-    plot_data(ax, 'kiss_tables/AMS-02_H_rigidity.txt', 2.7, 1., 'o', 'tab:blue', 'AMS-02')
+    plot_data(ax, 'kiss_tables/AMS-02_H_rigidity.txt', 2.7, 1., 'o', 'tab:olive', 'AMS-02')
     plot_data(ax, 'kiss_tables/CALET_H_kineticEnergy.txt', 2.7, 1., 'o', 'tab:orange', 'CALET', 3)
     plot_data(ax, 'kiss_tables/DAMPE_H_totalEnergy.txt', 2.7, 1., 'o', 'tab:red', 'DAMPE', 3)
 
