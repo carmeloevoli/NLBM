337d472551a1f40fd6e8d5b26272fd543ccf8f69
carmeloevoli <carmelo.evoli@gssi.it>
2023-09-09 13:30:37 +0200

    compute sec

--- compute_secondary.py
+++ compute_secondary.py
@@ -4,6 +4,28 @@ import matplotlib.pyplot as plt
 plt.style.use('gryphon.mplstyle')
 import numpy as np
 import math
+from dataclasses import dataclass
+
+from scipy.integrate import quad
+from scipy.interpolate import RegularGridInterpolator
+
+# FIXED PARAMS
+
+year = 3.1e7 # s
+Myr = 1e6 * year # s
+pc = 3.1e18 # cm
+kpc = 1e3 * pc # cm
+cLight = 3e10 # cm / s
+snrate = 1. / 50 / year # 1/s
+hdisk = 100. * pc # cm
+R_G = 10. * kpc # cm
+V_G = math.pi * np.power(R_G, 2.) * 2. * hdisk # cm^3
+E_0 = 10. # GeV
+sigmaCB = 60e-27 # cm^2
+n_H = 1. # 1/cm^3
+n_c = 50. # 1/cm^3
+E_CR = 1e51 # GeV
+mbarn = 1e-27 # cm^2
 
 def plot_data(ax, filename, slope, norm, fmt, color, label, zorder=1):
     R, y, err_stat_lo, err_stat_up, err_sys_lo, err_sys_up = np.loadtxt(filename,usecols=(0,1,2,3,4,5),unpack=True)
@@ -13,39 +35,180 @@ def plot_data(ax, filename, slope, norm, fmt, color, label, zorder=1):
     y_err_up = norm * np.power(E, slope) * np.sqrt(err_stat_up**2. + err_sys_up**2.)
     ax.errorbar(E, y, yerr=[y_err_lo, y_err_up], fmt=fmt, markeredgecolor=color, color=color, label=label,
                 capsize=4.5, markersize=7, elinewidth=2.2, capthick=2.2, zorder=zorder)
+
+def get_dsigmadE_ap():
+    Eproj_table, Epbar_table, pp_table, pHe_table, Hep_table, HeHe_table = np.loadtxt('data/supplementary__XS_table_Param_II_B.txt', usecols=(0,1,2,3,8,9), unpack=True)
+    projectile_kinetic_energy = np.linspace(np.log10(1.), np.log10(1e7), 211)
+    secondary_kinetic_energy = np.linspace(np.log10(0.1), np.log10(1e4), 151)
+    points = (secondary_kinetic_energy, projectile_kinetic_energy)
+
+    pp_xsec = np.zeros((len(secondary_kinetic_energy), len(projectile_kinetic_energy)))
+    pHe_xsec = np.zeros((len(secondary_kinetic_energy), len(projectile_kinetic_energy)))
+    Hep_xsec = np.zeros((len(secondary_kinetic_energy), len(projectile_kinetic_energy)))
+    HeHe_xsec = np.zeros((len(secondary_kinetic_energy), len(projectile_kinetic_energy)))
+    count = 0
+    for j in range(len(projectile_kinetic_energy)):
+        pp_xsec[:,j] = pp_table[count:count + 151] * 1e31 # m2 -> mbarn
+        pHe_xsec[:,j] = pHe_table[count:count + 151] * 1e31 # m2 -> mbarn
+        Hep_xsec[:,j] = Hep_table[count:count + 151] * 1e31 # m2 -> mbarn
+        HeHe_xsec[:,j] = HeHe_table[count:count + 151] * 1e31 # m2 -> mbarn
+        count = count + 151
+    pp_i = RegularGridInterpolator(points, pp_xsec, bounds_error=False, fill_value=0.)
+    pHe_i = RegularGridInterpolator(points, pHe_xsec, bounds_error=False, fill_value=0.)
+    Hep_i = RegularGridInterpolator(points, Hep_xsec, bounds_error=False, fill_value=0.)
+    HeHe_i = RegularGridInterpolator(points, HeHe_xsec, bounds_error=False, fill_value=0.)
+
+    return pp_i, pHe_i, Hep_i, HeHe_i
+
+def gest_dsigmadE_pos():
+    Eproj_table, Epbar_table, pp_table, pHe_table, Hep_table, HeHe_table = np.loadtxt('data/supplementary_table_positrons_best_fit.dat', usecols=(0,1,2,3,8,9), unpack=True)
+
+    projectile_kinetic_energy = np.linspace(np.log10(0.1), np.log10(1e6), 140)
+    secondary_kinetic_energy = np.linspace(np.log10(0.01), np.log10(1e4), 90)
+    points = (secondary_kinetic_energy, projectile_kinetic_energy)
+
+    pp_xsec = np.zeros((len(secondary_kinetic_energy), len(projectile_kinetic_energy)))
+    pHe_xsec = np.zeros((len(secondary_kinetic_energy), len(projectile_kinetic_energy)))
+    Hep_xsec = np.zeros((len(secondary_kinetic_energy), len(projectile_kinetic_energy)))
+    HeHe_xsec = np.zeros((len(secondary_kinetic_energy), len(projectile_kinetic_energy)))
+    count = 0
+    for j in range(len(projectile_kinetic_energy)):
+        pp_xsec[:,j] = pp_table[count:count + 90] # mbarn
+        pHe_xsec[:,j] = pHe_table[count:count + 90] # mbarn
+        Hep_xsec[:,j] = Hep_table[count:count + 90] # mbarn
+        HeHe_xsec[:,j] = HeHe_table[count:count + 90] # mbarn
+        count = count + 90
+    pp_i = RegularGridInterpolator(points, pp_xsec, bounds_error=False, fill_value=0.)
+    pHe_i = RegularGridInterpolator(points, pHe_xsec, bounds_error=False, fill_value=0.)
+    Hep_i = RegularGridInterpolator(points, Hep_xsec, bounds_error=False, fill_value=0.)
+    HeHe_i = RegularGridInterpolator(points, HeHe_xsec, bounds_error=False, fill_value=0.)
+
+    return pp_i, pHe_i, Hep_i, HeHe_i
+
+@dataclass
+class InjectionParams:
+    A: float
+    lnEb: float
+    p: float
+    dp: float
+    s: float
+    tauG: float
+
+@dataclass
+class GrammageParams:
+    tauG: float
+    tau0: float
+    zeta: float
+
+def primaryModel(E, par):
+    value = snrate * par.tauG / V_G # cm^-3
+    value *= E_CR / np.power(E_0, 2.) * (par.p - 2.) # GeV^-1 cm^-3
+    value *= cLight / 4. / math.pi # GeV^-1 cm^-2 s^-1 sr^-1
+    Eb = np.exp(par.lnEb)
+    y = par.A * value * (E / E_0)**(-par.p)
+    y *= np.power(1. + np.power(E / Eb, par.dp / par.s), par.s)
+    return y
+
+def I_ap():
+    Ip_par = InjectionParams(14.864, np.log(639.935), 2.811, 0.251, 0.123, 0.937 * Myr)
+    Ihe_par = InjectionParams(0.948, np.log(474.829), 2.737, 0.308, 0.192, 0.937 * Myr)
+        
+    chi_par = GrammageParams(0.937 * Myr, 0.079 * Myr, 0.080)
+    pp_xsec, pHe_xsec, Hep_xsec, HeHe_xsec = get_dsigmadE_ap()
+
+    def integrand_qg(lnEprime, E):
+        Eprime = np.exp(lnEprime)
+        pp_s = pp_xsec([np.log10(E), np.log10(Eprime)]) * mbarn
+        hep_s = Hep_xsec([np.log10(E), np.log10(Eprime)]) * mbarn
+        return Eprime * chi_par.tauG * (primaryModel(Eprime, Ip_par) * pp_s + primaryModel(Eprime, Ihe_par) * hep_s)
+
+    def integrand_qc(lnEprime, E):
+        Eprime = np.exp(lnEprime)
+        pp_s = pp_xsec([np.log10(E), np.log10(Eprime)]) * mbarn
+        hep_s = Hep_xsec([np.log10(E), np.log10(Eprime)]) * mbarn
+        tau_c = chi_par.tau0 * np.power(Eprime, -chi_par.zeta * np.log(Eprime))
+        return Eprime * tau_c * (primaryModel(Eprime, Ip_par) * pp_s + primaryModel(Eprime, Ihe_par) * hep_s)
+
+    size = 100
+    E = np.logspace(0, 4, size)
+    value_G = np.zeros(size)
+    for i in range(size):
+        value_G[i] = quad(integrand_qg, np.log(E[i]), np.log(1e4 * E[i]), args=(E[i]))[0]
+    value_G *= cLight * n_H
+    
+    value_c = np.zeros(size)
+    for i in range(size):
+        value_c[i] = quad(integrand_qc, np.log(E[i]), np.log(1e4 * E[i]), args=(E[i]))[0]
+    value_c *= cLight * n_c
+
+    return E, value_c, value_G
+
+def I_pos():
+    Ip_par = InjectionParams(14.864, np.log(639.935), 2.811, 0.251, 0.123, 0.937 * Myr)
+    Ihe_par = InjectionParams(0.948, np.log(474.829), 2.737, 0.308, 0.192, 0.937 * Myr)
+        
+    chi_par = GrammageParams(0.937 * Myr, 0.079 * Myr, 0.080)
+    pp_xsec, pHe_xsec, Hep_xsec, HeHe_xsec = gest_dsigmadE_pos()
+
+    def integrand_qg(lnEprime, E):
+        Eprime = np.exp(lnEprime)
+        pp_s = pp_xsec([np.log10(E), np.log10(Eprime)]) * mbarn
+        hep_s = Hep_xsec([np.log10(E), np.log10(Eprime)]) * mbarn
+        return Eprime * chi_par.tauG * (primaryModel(Eprime, Ip_par) * pp_s + primaryModel(Eprime, Ihe_par) * hep_s)
+
+    def integrand_qc(lnEprime, E):
+        Eprime = np.exp(lnEprime)
+        pp_s = pp_xsec([np.log10(E), np.log10(Eprime)]) * mbarn
+        hep_s = Hep_xsec([np.log10(E), np.log10(Eprime)]) * mbarn
+        tau_c = chi_par.tau0 * np.power(Eprime, -chi_par.zeta * np.log(Eprime))
+        return Eprime * tau_c * (primaryModel(Eprime, Ip_par) * pp_s + primaryModel(Eprime, Ihe_par) * hep_s)
+
+    size = 100
+    E = np.logspace(0, 4, size)
+    value_G = np.zeros(size)
+    for i in range(size):
+        value_G[i] = quad(integrand_qg, np.log(E[i]), np.log(1e4 * E[i]), args=(E[i]))[0]
+    value_G *= cLight * n_H
+    
+    value_c = np.zeros(size)
+    for i in range(size):
+        value_c[i] = quad(integrand_qc, np.log(E[i]), np.log(1e4 * E[i]), args=(E[i]))[0]
+    value_c *= cLight * n_c
+
+    return E, value_c, value_G
     
 def plot_antiprotons():
     def set_axes(ax):
         ax.set_xlabel('E [GeV]')
         ax.set_xscale('log')
         ax.set_xlim([1e1, 1e3])
-        ax.set_ylabel(r'E$^{2.7}$ I')
-        ax.set_ylim([0., 7.])
+        ax.set_ylabel(r'E$^{2.7}$ I [GeV$^{1.7}$ m$^{-2}$ s$^{-1}$ sr$^{-1}$]')
+        ax.set_ylim([0.5, 10.])
+        ax.set_yscale('log')
 
     fig = plt.figure(figsize=(10.5, 8.5))
     ax = fig.add_subplot(111)
     set_axes(ax)
 
-    ax.fill_between([10,20], 0, 7, color='tab:gray', alpha=0.2)
+    #ax.fill_between([10,20], 0, 7, color='tab:gray', alpha=0.2)
 
-    plot_data(ax, 'kiss_tables/AMS-02_pbar_rigidity.txt', 2.7, 1., 'o', 'darkorange', r'$\bar p$', 1)
-    plot_data(ax, 'kiss_tables/AMS-02_e+_rigidity.txt', 2.7, 1., 'o', 'red', r'$e^+$', 1)
+    plot_data(ax, 'kiss_tables/AMS-02_pbar_rigidity.txt', 2.7, 1., 'o', 'red', r'$\bar p$', 1)
+    plot_data(ax, 'kiss_tables/AMS-02_e+_rigidity.txt', 2.7, 1., 'o', 'blue', r'$e^+$', 1)
+    
+    f_He = 1.2
+    
+    E, I_c, I_G = I_ap()
+    ax.plot(E, np.power(E, 2.7) * I_G * f_He * 1e4, 'darkorange', linestyle='--') # cm -> m
+    ax.plot(E, np.power(E, 2.7) * (I_G + I_c) * f_He * 1e4, 'darkorange', linestyle='-')
 
+    E, I_c, I_G = I_pos()
+    ax.plot(E, np.power(E, 2.7) * I_G * f_He * 1e4, 'tab:cyan', linestyle='--') # cm -> m
+    ax.plot(E, np.power(E, 2.7) * (I_G + I_c) * f_He * 1e4, 'tab:cyan', linestyle='-')
 
-#    plot_data(ax, 'kiss_tables/AMS-02_H_rigidity.txt', 2.7, 1.8e-4, 'o', 'lightgray', 'protons [1.8 x 10$^{-4}$]')
-#    plot_data(ax, 'kiss_tables/CALET_H_kineticEnergy.txt', 2.7, 1., 'o', 'tab:orange', 'CALET', 3)
-#    plot_data(ax, 'kiss_tables/DAMPE_H_totalEnergy.txt', 2.7, 1., 'o', 'tab:red', 'DAMPE', 3)
-
-#    E, I_c, I_G = I_ap()
-#    I_G *= 1.9
-#    I_c *= 5.6
-#    ax.plot(E, np.power(E, 2.7) * I_G, 'tab:gray', linestyle='--')
-#    ax.plot(E, np.power(E, 2.7) * I_c, 'tab:gray', linestyle=':')
-#    ax.plot(E, np.power(E, 2.7) * (I_G + I_c), 'tab:gray', linestyle='-')
-
-    ax.legend(fontsize=30, loc='upper right')
+    ax.legend(fontsize=30, loc='best')
     plt.savefig('NLBM_ap.pdf')
 
 if __name__== "__main__":
     plot_antiprotons()
     #plot_positrons()
+
--- fit_model.py
+++ fit_model.py
@@ -21,12 +21,9 @@ R_G = 10. * kpc # cm
 V_G = math.pi * np.power(R_G, 2.) * 2. * hdisk # cm^3
 E_0 = 10. # GeV
 sigmaCB = 60e-27 # cm^2
-zeta = 0.1
 n_H = 1. # 1/cm^3
 n_c = 50. # 1/cm^3
-E_CR = 1e55 # GeV
-
-tau_G = 29.0e12 # s
+E_CR = 1e51 # GeV
 
 def plot_data(ax, filename, slope, norm, fmt, color, label, zorder=1):
     R, y, err_stat_lo, err_stat_up, err_sys_lo, err_sys_up = np.loadtxt(filename,usecols=(0,1,2,3,4,5),unpack=True)
@@ -38,19 +35,26 @@ def plot_data(ax, filename, slope, norm, fmt, color, label, zorder=1):
                 capsize=4.5, markersize=7, elinewidth=2.2, capthick=2.2, zorder=zorder)
 
 @dataclass
-class ModelParams:
+class InjectionParams:
     A: float
     lnEb: float
     p: float
     dp: float
     s: float
+    tauG: float
 
-def model(E, par):
-    value = cLight / 4. / math.pi
-    value *= snrate * tau_G / V_G
-    value *= E_CR / np.power(E_0, 2.) * (par.p - 2.)
+@dataclass
+class GrammageParams:
+    tauG: float
+    tau0: float
+    zeta: float
+
+def primaryModel(E, par):
+    value = snrate * par.tauG / V_G # cm^-3
+    value *= E_CR / np.power(E_0, 2.) * (par.p - 2.) # GeV^-1 cm^-3
+    value *= cLight / 4. / math.pi # GeV^-1 cm^-2 s^-1 sr^-1
     Eb = np.exp(par.lnEb)
-    y = par.A * (E / E_0)**(-par.p)
+    y = par.A * value * (E / E_0)**(-par.p)
     y *= np.power(1. + np.power(E / Eb, par.dp / par.s), par.s)
     return y
 
@@ -69,23 +73,23 @@ def fit_H(par):
         xd, yd, errd_lo, errd_up = get_data(filename, norm, 30., 1e4)
         chi2 = 0.
         for x_i, y_i, err_lo_i, err_up_i in zip(xd, yd, errd_lo, errd_up):
-            Y = model(x_i, par)
+            Y = 1e4 * primaryModel(x_i, par) # GeV^-1 m^-2 s^-1 sr^-1
             if Y > y_i:
                 chi2 += np.power((Y - y_i) / err_up_i, 2.)
             else:
                 chi2 += np.power((Y - y_i) / err_lo_i, 2.)
         return chi2
  
-    def chi2_function(A, lnEb, p, dp, s):
+    def chi2_function(A, lnEb, p, dp, s, tauG):
         chi2 = 0.
-        chi2 += experiment_chi2('kiss_tables/AMS-02_H_rigidity.txt', ModelParams(A, lnEb, p, dp, s), 1.)
-        chi2 += experiment_chi2('kiss_tables/DAMPE_H_totalEnergy.txt', ModelParams(A, lnEb, p, dp, s), 1.)
-        chi2 += experiment_chi2('kiss_tables/CALET_H_kineticEnergy.txt', ModelParams(A, lnEb, p, dp, s), 1.)
+        chi2 += experiment_chi2('kiss_tables/AMS-02_H_rigidity.txt', InjectionParams(A, lnEb, p, dp, s, tauG), 1.)
+        chi2 += experiment_chi2('kiss_tables/DAMPE_H_totalEnergy.txt', InjectionParams(A, lnEb, p, dp, s, tauG), 1.)
+        chi2 += experiment_chi2('kiss_tables/CALET_H_kineticEnergy.txt', InjectionParams(A, lnEb, p, dp, s, tauG), 1.)
         return chi2
 
-    m = Minuit(chi2_function, A=par.A, lnEb=par.lnEb, p=par.p, dp=par.dp, s=par.s)
+    m = Minuit(chi2_function, A=par.A, lnEb=par.lnEb, p=par.p, dp=par.dp, s=par.s, tauG=par.tauG)
     m.errordef = Minuit.LEAST_SQUARES
-    #m.fixed['s'] = True
+    m.fixed['tauG'] = True
     m.limits['s'] = (0.01, 1.)
     
     m.simplex()
@@ -95,12 +99,13 @@ def fit_H(par):
     print(m.params)
 
     E = np.logspace(1, 9, 1000)
-    y = model(E, ModelParams(m.values[0], m.values[1], m.values[2], m.values[3], m.values[4]))
-    y, ycov = propagate(lambda z: model(E, ModelParams(z[0], z[1], z[2], z[3], z[4])), m.values, m.covariance)
+    values = InjectionParams(m.values[0], m.values[1], m.values[2], m.values[3], m.values[4], m.values[5])
+    y = primaryModel(E, values)
+    y, ycov = propagate(lambda z: primaryModel(E, InjectionParams(z[0], z[1], z[2], z[3], z[4], z[5])), m.values, m.covariance)
 
-    return E, y, np.sqrt(np.diag(ycov))
+    return E, y, 2. * np.sqrt(np.diag(ycov)), values
     
-def plot_H():
+def plot_H(initialValues):
     def set_axes(ax):
         ax.set_xlabel('E [GeV]')
         ax.set_xscale('log')
@@ -122,12 +127,14 @@ def plot_H():
     filename = 'kiss_tables/CALET_H_kineticEnergy.txt'
     plot_data(ax, filename, 2.7, 1., 'o', 'royalblue', 'CALET', 3)
 
-    E, y, ycov = fit_H(ModelParams(30., np.log(1e3), 2.85, 0.25, 0.1))
-    ax.plot(E, np.power(E, 2.7) * y, color='tab:purple', zorder=10)
-    ax.fill_between(E, np.power(E, 2.7) * (y - ycov), np.power(E, 2.7) * (y + ycov), color='tab:purple', alpha=0.25, zorder=10)
+    E, y, ycov, values = fit_H(initialValues)
+    ax.plot(E, np.power(E, 2.7) * 1e4 * y, color='tab:purple', zorder=10)
+    ax.fill_between(E, np.power(E, 2.7) * 1e4 * (y - ycov), np.power(E, 2.7) * 1e4 * (y + ycov), color='tab:purple', alpha=0.25, zorder=10)
 
     ax.legend(fontsize=18, loc='upper left')
     plt.savefig('NLBM_H.pdf')
+    
+    return values
 
 def fit_He(par):
     def get_data(filename, norm, minEnergy, maxEnergy = 1e20):
@@ -144,23 +151,23 @@ def fit_He(par):
         xd, yd, errd_lo, errd_up = get_data(filename, norm, 30., 1e4)
         chi2 = 0.
         for x_i, y_i, err_lo_i, err_up_i in zip(xd, yd, errd_lo, errd_up):
-            Y = model(x_i, par)
+            Y = 1e4 * primaryModel(x_i, par) # GeV^-1 m^-2 s^-1 sr^-1
             if Y > y_i:
                 chi2 += np.power((Y - y_i) / err_up_i, 2.)
             else:
                 chi2 += np.power((Y - y_i) / err_lo_i, 2.)
         return chi2
  
-    def chi2_function(A, lnEb, p, dp, s):
+    def chi2_function(A, lnEb, p, dp, s, tauG):
         chi2 = 0.
-        chi2 += experiment_chi2('kiss_tables/AMS-02_He_rigidity.txt', ModelParams(A, lnEb, p, dp, s), 2.)
-        chi2 += experiment_chi2('kiss_tables/DAMPE_He_totalEnergy.txt', ModelParams(A, lnEb, p, dp, s), 4.)
-        chi2 += experiment_chi2('kiss_tables/CALET_He_kineticEnergy.txt', ModelParams(A, lnEb, p, dp, s), 4.)
+        chi2 += experiment_chi2('kiss_tables/AMS-02_He_rigidity.txt', InjectionParams(A, lnEb, p, dp, s, tauG), 2.)
+        chi2 += experiment_chi2('kiss_tables/DAMPE_He_totalEnergy.txt', InjectionParams(A, lnEb, p, dp, s, tauG), 4.)
+        chi2 += experiment_chi2('kiss_tables/CALET_He_kineticEnergy.txt', InjectionParams(A, lnEb, p, dp, s, tauG), 4.)
         return chi2
 
-    m = Minuit(chi2_function, A=par.A, lnEb=par.lnEb, p=par.p, dp=par.dp, s=par.s)
+    m = Minuit(chi2_function, A=par.A, lnEb=par.lnEb, p=par.p, dp=par.dp, s=par.s, tauG=par.tauG)
     m.errordef = Minuit.LEAST_SQUARES
-    #m.fixed['s'] = True
+    m.fixed['tauG'] = True
     m.limits['s'] = (0.01, 1.)
 
     m.simplex()
@@ -170,12 +177,13 @@ def fit_He(par):
     print(m.params)
 
     E = np.logspace(1, 9, 1000)
-    y = model(E, ModelParams(m.values[0], m.values[1], m.values[2], m.values[3], m.values[4]))
-    y, ycov = propagate(lambda z: model(E, ModelParams(z[0], z[1], z[2], z[3], z[4])), m.values, m.covariance)
+    values = InjectionParams(m.values[0], m.values[1], m.values[2], m.values[3], m.values[4], m.values[5])
+    y = primaryModel(E, values)
+    y, ycov = propagate(lambda z: primaryModel(E, InjectionParams(z[0], z[1], z[2], z[3], z[4], z[5])), m.values, m.covariance)
 
-    return E, y, np.sqrt(np.diag(ycov))
+    return E, y, 2. * np.sqrt(np.diag(ycov)), values
 
-def plot_He():
+def plot_He(initialValues):
     def set_axes(ax):
         ax.set_xlabel('E [GeV/n]')
         ax.set_xscale('log')
@@ -197,21 +205,17 @@ def plot_He():
     filename = 'kiss_tables/CALET_He_kineticEnergy.txt'
     plot_data(ax, filename, 2.7, 4., 'o', 'royalblue', 'CALET', 3)
 
-    E, y, ycov = fit_He(ModelParams(30., np.log(1e3), 2.85, 0.25, 0.1))
-    ax.plot(E, np.power(E, 2.7) * y, color='tab:purple', zorder=10)
-    ax.fill_between(E, np.power(E, 2.7) * (y - ycov), np.power(E, 2.7) * (y + ycov), color='tab:purple', alpha=0.25, zorder=10)
+    E, y, ycov, values = fit_He(initialValues)
+    ax.plot(E, np.power(E, 2.7) * 1e4 * y, color='tab:purple', zorder=10)
+    ax.fill_between(E, np.power(E, 2.7) * 1e4 * (y - ycov), np.power(E, 2.7) * 1e4 * (y + ycov), color='tab:purple', alpha=0.25, zorder=10)
 
     ax.legend(fontsize=18, loc='upper left')
     plt.savefig('NLBM_He.pdf')
 
-@dataclass
-class GrammageParams:
-    tauG: float
-    tau0: float
-    zeta: float
+    return values
 
 def grammage(E, par):
-    value_G = cLight * sigmaCB * par.tauG * n_H
+    value_G = cLight * sigmaCB * n_H * par.tauG
     value_c = cLight * sigmaCB * n_c * par.tau0 * np.power(E, -par.zeta * np.log(E))
     return value_c + value_G
 
@@ -246,22 +250,21 @@ def fit_BC(par):
 
     m = Minuit(chi2_function, tauG=par.tauG, tau0=par.tau0, zeta=par.zeta)
     m.errordef = Minuit.LEAST_SQUARES
-    #m.fixed['s'] = True
-    #m.limits['s'] = (0.01, 1.)
 
     m.simplex()
     m.migrad()
     m.minos()
 
     print(m.params)
-
+    
     E = np.logspace(1, 9, 1000)
-    y = grammage(E, GrammageParams(m.values[0], m.values[1], m.values[2]))
+    values = GrammageParams(m.values[0], m.values[1], m.values[2])
+    y = grammage(E, values)
     y, ycov = propagate(lambda z: grammage(E, GrammageParams(z[0], z[1], z[2])), m.values, m.covariance)
 
-    return E, y, np.sqrt(np.diag(ycov))
-    
-def plot_BC():
+    return E, y, 2. * np.sqrt(np.diag(ycov)), values
+
+def plot_BC(initialValues):
     def set_axes(ax):
         ax.set_xlabel('E [GeV/n]')
         ax.set_xscale('log')
@@ -278,14 +281,34 @@ def plot_BC():
     plot_data(ax, 'kiss_tables/CALET_B_C_kineticEnergyPerNucleon.txt', 0., 1., 'o', 'limegreen', 'DAMPE', 2)
     plot_data(ax, 'kiss_tables/DAMPE_B_C_kineticEnergyPerNucleon.txt', 0., 1., 'o', 'royalblue', 'CALET', 3)
 
-    E, y, ycov = fit_BC(GrammageParams(Myr, 0.1 * Myr, 0.1))
+    E, y, ycov, values = fit_BC(initialValues)
     ax.plot(E, y, color='tab:purple', zorder=10)
     ax.fill_between(E, (y - ycov), (y + ycov), color='tab:purple', alpha=0.25, zorder=10)
 
-    ax.legend(fontsize=18, loc='lower left')
+    ax.hlines(cLight * sigmaCB * values.tauG * n_H, 1, 1e5, color='tab:purple', linestyle='--')
+
+    ax.legend(fontsize=18, loc='upper right')
     plt.savefig('NLBM_BC.pdf')
+    
+    return values
 
 if __name__== "__main__":
-    plot_BC()
-    plot_H()
-    plot_He()
+    chiPar = plot_BC(GrammageParams(Myr, 0.1 * Myr, 0.1))
+    qhPar = plot_H(InjectionParams(30., np.log(1e3), 2.85, 0.25, 0.1, chiPar.tauG))
+    qhePar = plot_He(InjectionParams(2., np.log(1e3), 2.75, 0.25, 0.1, chiPar.tauG))
+
+    print(f'tau_G = {chiPar.tauG/Myr:5.3f} Myr')
+    print(f'tau_0 = {chiPar.tau0/Myr:5.3f} Myr')
+    print(f'zeta = {chiPar.zeta:5.3f}')
+    
+    print(f'H_A = {qhPar.A:5.3f}')
+    print(f'H_Eb = {np.exp(qhPar.lnEb):5.3f} GeV')
+    print(f'H_p = {qhPar.p:5.3f}')
+    print(f'H_dp = {qhPar.dp:5.3f}')
+    print(f'H_s = {qhPar.s:5.3f}')
+
+    print(f'He_A = {qhePar.A:5.3f}')
+    print(f'He_Eb = {np.exp(qhePar.lnEb):5.3f} GeV')
+    print(f'He_p = {qhePar.p:5.3f}')
+    print(f'He_dp = {qhePar.dp:5.3f}')
+    print(f'He_s = {qhePar.s:5.3f}')
